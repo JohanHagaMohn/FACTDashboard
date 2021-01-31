84846498a828862527e81b8d67f500afb5850365
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2021-01-31 17:04:01 +0100

    Staretd at a python script to convert data

--- routes/API/neo4j.js
+++ routes/API/neo4j.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const data = require("./neo4jTestData.json")
+const data = require("./neo4jTestData/data.json")
 
 router.get("/example", (req, res, next) => {
   res.json(data)
