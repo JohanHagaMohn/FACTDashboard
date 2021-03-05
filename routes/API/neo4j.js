0ab838ecf5a43fdf505085b159befa0693610960
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2021-03-05 23:32:36 +0100

    Fixed a bug

--- routes/API/neo4j.js
+++ routes/API/neo4j.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const neo4j = require('neo4j-driver')
 
-//const data = require("./neo4jTestData/data.json")
+const data = require("./neo4jTestData/data.json")
 
 // env
 const username = process.env.NEO4J_DB_USERNAME;
@@ -10,15 +10,20 @@ const password = process.env.NEO4J_DB_PASSWORD;
 const uri = process.env.NEO4J_DB_URI
 
 console.log("Connecting to Neo4j")
+let driver, session
 try {
-  const driver = neo4j.driver(uri, neo4j.auth.basic(username, password))
-  const session = driver.session()
+  driver = neo4j.driver(uri, neo4j.auth.basic(username, password))
+  session = driver.session()
 } catch (err) {
   console.warn("Failed to connect to Neo4j")
   console.log(err)
 }
 
 router.get("/example", async (req, res, next) => {
+  res.json(data)
+})
+
+router.get("/example2", async (req, res, next) => {
   const result = await session.run('MATCH (n) RETURN n')
   console.log(result)
   res.json(result)
