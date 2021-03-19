38499a3dbb8eb7b8d757d9f2118369c321f231b0
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2021-03-19 19:29:27 +0100

    Added a apiendpoin to recieve data from neo4j

--- routes/API/neo4j.js
+++ routes/API/neo4j.js
@@ -24,9 +24,19 @@ router.get("/example", async (req, res, next) => {
 })
 
 router.get("/example2", async (req, res, next) => {
-  const result = await session.run('MATCH (n) RETURN n')
-  console.log(result)
+  const result = await session.run('MATCH (n:tweet)-[r:retweet]->() RETURN *')
   res.json(result)
+})
+
+router.get("/setup", async (req, res, next) => {
+  res.send("hello")
+  for (let i = 0; i < data.users.length; i++) {
+    const result = await session.run("CREATE (n:tweet {id_str: $id_str, name: $name, screen_name: $screen_name, description: $description, created_at: $created_at}) RETURN n", data.users[i])
+  }
+
+  for(let edge of data.edges) {
+    const result = await session.run("MATCH (a:tweet {id_str: $source}) MATCH (b:tweet {id_str: $target}) CREATE (a)-[:retweet]->(b)", edge)
+  }
 })
 
 module.exports = router;
