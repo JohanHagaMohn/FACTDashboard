8603731c061096b74f99ac9d107e9199d07e6b4e
Theodor Kvalsvik Lauritzen <theodor.k.lauritzen@gmail.com>
2021-09-05 16:04:44 +0200

    Paused, wil come back here

---
+++ neo4j.js
@@ -0,0 +1,15 @@
+const neo4j = require('neo4j-driver')
+
+// env
+const username = process.env.NEO4J_DB_USERNAME;
+const password = process.env.NEO4J_DB_PASSWORD;
+const uri = process.env.NEO4J_DB_URI
+
+let driver, session
+try {
+  driver = neo4j.driver(uri, neo4j.auth.basic(username, password))
+  session = driver.session()
+} catch (err) {
+  console.warn("Failed to connect to Neo4j")
+  console.log(err)
+}
