60dfe3191acabce1f627a269490c390561be691b
Johan Haga Mohn <47793383+JohanHagaMohn@users.noreply.github.com>
2021-09-05 15:45:57 +0200

    mongo Import

--- routes/API/neo4jTestData/importMongo.py
+++ routes/API/neo4jTestData/importMongo.py
@@ -1,13 +1,16 @@
 import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="../../../.env")
 
 
 def mongoConnect():
-    USERNAME = os.environ["MONGO_DB_USERNAME"]
-    PASSWORD = os.environ["MONGO_DB_PASSWORD"]
-    DBIP = os.environ["MONGO_DB_IP"]
-    DBNAME = os.environ["MONGO_DB_DBNAME"]
-    DBPORT = os.environ["MONGO_DB_PORT"]
+    USERNAME = os.getenv("MONGO_DB_USERNAME")
+    PASSWORD = os.getenv("MONGO_DB_PASSWORD")
+    DBIP = os.getenv("MONGO_DB_IP")
+    DBNAME = os.getenv("MONGO_DB_DBNAME")
+    DBPORT = os.getenv("MONGO_DB_PORT")
     DBURL = f"mongodb://{USERNAME}:{PASSWORD}@{DBIP}:{DBPORT}/{DBNAME}?authMechanism=DEFAULT"
     client = MongoClient(DBURL)
     print("Connected successfully to Mongo DB")
@@ -15,4 +18,16 @@ def mongoConnect():
     tweetCol = DB.collection["tweets"]
     userCol = DB.collection["users"]
 
-    print(client, "\n", DB.list_collection_names(), tweetCol.find_one({}))
+    print(
+        client,
+        "\n",
+        DB.list_collection_names(),
+        tweetCol.find_one({"id_str": "1295320924938481667"}),
+        tweetCol.count_documents({}),
+    )
+    import pprint
+
+    pprint.pprint(tweetCol.find_one())
+
+
+mongoConnect()
